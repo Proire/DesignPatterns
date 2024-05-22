0392c406337f031fff4995ee2c21ad46839524f9
Proire <prasadahire4501@gmail.com>
2024-05-22 08:02:44 +0530

    [Prasad] Add : Added Four Classes Representing Fascade design pattern and used in Program class

--- FascadeDesignPattern/Program.cs
+++ FascadeDesignPattern/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("------------- Fascade Design Pattern -------------");
+
+            //The Client will use the Facade Interface instead of the Subsystems
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
         }
     }
 }
